7e7195ba0e4fea57e04f02e80f07e927d2136e81
Antonio Trovato <atrovato@unisa.it>
2025-10-05 18:00:22 +0200

    adding the other comments as required by the integrator!

--- src/test/java/com/example/GreeterTest.java
+++ src/test/java/com/example/GreeterTest.java
@@ -7,10 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GreeterTest {
     @Test
     void greet_withName() {
+        // adding comment
         assertEquals("Hello, Alice!", new Greeter().greet("Alice"));
     }
     @Test
     void greet_default() {
+        // adding comment
         assertEquals("Hello, world!", new Greeter().greet(""));
     }
 }
